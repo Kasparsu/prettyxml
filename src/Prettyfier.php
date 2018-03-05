c375af70a6584be1a0c967e4d6e329a26f5613db
Kaspar Martin Suursalu <kasparsu@gmail.com>
2018-03-05 22:57:07 +0200

    add constructor, getters and setters

--- src/Prettyfier.php
+++ src/Prettyfier.php
@@ -5,5 +5,53 @@ namespace Kasparsu\PrettyXml;
 
 class Prettyfier
 {
+    /**
+     * @var string
+     */
+    private $contents;
+    /**
+     * @var string
+     */
+    private $indentation;
 
+    /**
+     * Prettyfier constructor.
+     */
+    public function __construct($contents, $indentation = '    ')
+    {
+        $this->indentation = $indentation;
+        $this->contents = $contents;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContents()
+    {
+        return $this->contents;
+    }
+
+    /**
+     * @param string $contents
+     */
+    public function setContents($contents)
+    {
+        $this->contents = $contents;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIndentation()
+    {
+        return $this->indentation;
+    }
+
+    /**
+     * @param string $indentation
+     */
+    public function setIndentation($indentation)
+    {
+        $this->indentation = $indentation;
+    }
 }
