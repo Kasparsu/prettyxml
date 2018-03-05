9450d785c900135d6a14b945f374de6da0625d58
Kaspar Martin Suursalu <kasparsu@gmail.com>
2018-03-06 00:01:41 +0200

    add setter getter and constructor tests

--- tests/XmlParserTest.php
+++ tests/XmlParserTest.php
@@ -7,7 +7,11 @@ use Kasparsu\PrettyXml\XmlParser;
 
 final class XmlParserTest extends TestCase
 {
+    /**
+     * @var XmlParser
+     */
     private $xmlParser;
+
     public function setUp()
     {
         $this->xmlParser = new XmlParser();
@@ -16,13 +20,27 @@ final class XmlParserTest extends TestCase
     /**
      * @dataProvider XmlProvider
      */
-    public function testIfFirstElementObjectIsCorrect($xml)
-    {
+    public function testIfConstructorInputIsReturnedInGetter($xml) {
+        $xmlParser = new XmlParser($xml);
+        $this->assertEquals($xml, $xmlParser->getXml());
+    }
+
+    /**
+     * @dataProvider XmlProvider
+     */
+    public function testIfSetterInputIsReturnedInGetter($xml) {
+        $this->xmlParser->setXml($xml);
+        $this->assertEquals($xml, $this->xmlParser->getXml());
+    }
+
+    /**
+     * @dataProvider XmlProvider
+     */
+    public function testIfFirstElementObjectIsCorrect($xml) {
        $this->xmlParser->setXml = $xml;
     }
 
-    public function XmlProvider()
-    {
+    public function XmlProvider() {
         return [
             'one tag only' => ['<test></test>'],
             ];
