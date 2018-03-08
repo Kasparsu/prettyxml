5d07d30d29cd2bfc2208b43dbe1a25863b626b96
Kaspar Martin Suursalu <kasparsu@gmail.com>
2018-03-08 22:36:53 +0200

    add xmlElement model

---
+++ src/XmlElement.php
@@ -0,0 +1,99 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaspa
+ * Date: 06/03/2018
+ * Time: 00:06
+ */
+
+namespace Kasparsu\PrettyXml;
+
+
+class XmlElement
+{
+    /**
+     * @var string
+     */
+    private $tag;
+    /**
+     * @var array
+     */
+    private $children = [];
+    /**
+     * @var array
+     */
+    private $attributes = [];
+
+    private $value = "";
+
+    /**
+     * XmlElement constructor.
+     * @param string $tag
+     * @param array $children
+     * @param array $attributes
+     */
+    public function __construct(string $tag, array $children = [], array $attributes = [])
+    {
+        $this->tag = $tag;
+        $this->children = $children;
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTag(): string
+    {
+        return $this->tag;
+    }
+
+    /**
+     * @return array
+     */
+    public function getChildren(): array
+    {
+        return $this->children;
+    }
+
+    /**
+     * @return array
+     */
+    public function getAttributes(): array
+    {
+        return $this->attributes;
+    }
+
+    /**
+     * @param array $children
+     */
+    public function setChildren(array $children)
+    {
+        $this->children = $children;
+    }
+
+    /**
+     * @param array $attributes
+     */
+    public function setAttributes(array $attributes)
+    {
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue(string $value)
+    {
+        $this->value = $value;
+    }
+
+
+}
---
+++ tests/PrettyfierTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaspa
+ * Date: 08/03/2018
+ * Time: 22:06
+ */
+
+namespace Tests;
+
+use Kasparsu\PrettyXml\Prettyfier;
+
+class PrettyfierTest extends \PHPUnit_Framework_TestCase
+{
+
+}
