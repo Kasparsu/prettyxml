f134fe08aaeccaba95c02e28a1f608c91fe78b12
Kaspar Martin Suursalu <kasparsu@gmail.com>
2018-03-08 22:40:25 +0200

    add logic for parsing prettyfied output from XmlElement model + tests

--- src/Prettyfier.php
+++ src/Prettyfier.php
@@ -56,5 +56,31 @@ class Prettyfier
     }
 
     public function prettify(){
+        $this->parser->parse();
+        return $this->createFormatedXmlFromElement($this->parser->getRootElement());
+    }
+
+    private function createFormatedXmlFromElement(XmlElement $element, $output="", $level = 0){
+        if($level){
+            $output .="\n" . str_repeat($this->indentation, $level);
+        }
+        $output .= "<{$element->getTag()}";
+        foreach($element->getAttributes() as $key=>$value) {
+            $output .= " " . $key . "=";
+            if(gettype ($value) == "string") {
+                $output .= '"' . $value . '"';
+            } else {
+                $output .= $value;
+            }
+        }
+        $output .= ">";
+        foreach($element->getChildren() as $child){
+           $output = $this->createFormatedXmlFromElement($child, $output, $level+1);
+        }
+        $output .= $element->getValue();
+        if($element->getValue() == "")
+            $output .="\n" . str_repeat($this->indentation, $level);
+        $output .= "</{$element->getTag()}>";
+        return $output;
     }
 }
--- tests/PrettyfierTest.php
+++ tests/PrettyfierTest.php
@@ -1,16 +1,34 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kaspa
- * Date: 08/03/2018
- * Time: 22:06
- */
 
 namespace Tests;
 
 use Kasparsu\PrettyXml\Prettyfier;
+use PHPUnit\Framework\TestCase;
 
-class PrettyfierTest extends \PHPUnit_Framework_TestCase
+class PrettyfierTest extends TestCase
 {
 
+    /**
+     * @dataProvider PrettyfierProvider
+     */
+    public function testIfPrettifierWorksCorrectly($xml, $indentation, $expectedOutput){
+        $prettyfier = new Prettyfier($xml, $indentation);
+        $this->assertEquals($expectedOutput, $prettyfier->prettify());
+    }
+
+    public function PrettyfierProvider() {
+        return [
+            'one tag only' => ['<test></test>', "\t", "<test>\n</test>"],
+            'exercise example' => [
+                '<apply><csymbol encoding="OpenMath"><msub><mi>P</mi><mn>1</mn></msub></csymbol><ci>x</ci></apply>',
+                "\t",
+                "<apply>\n\t<csymbol encoding=\"OpenMath\">\n\t\t<msub>\n\t\t\t<mi>P</mi>\n\t\t\t<mn>1</mn>\n\t\t</msub>\n\t</csymbol>\n\t<ci>x</ci>\n</apply>"
+                ],
+            'different indetation' => [
+                '<test><a>a</a><b>b</b><n>c</n></test>',
+                '    ',
+                "<test>\n    <a>a</a>\n    <b>b</b>\n    <n>c</n>\n</test>"
+                ],
+        ];
+    }
 }
