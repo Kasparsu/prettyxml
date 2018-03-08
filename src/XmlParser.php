aabe39434c4ce2ce744b52099ee5b380e9fe6dd2
Kaspar Martin Suursalu <kasparsu@gmail.com>
2018-03-08 22:39:16 +0200

    add logic for tag, child, attribute and value detection and creating XmlElement model + tests

--- src/XmlParser.php
+++ src/XmlParser.php
@@ -5,7 +5,14 @@ namespace Kasparsu\PrettyXml;
 
 class XmlParser
 {
-    private $xmlArray = [];
+    /**
+     * @var XmlElement
+     */
+    private $rootElement;
+
+    /**
+     * @var string
+     */
     private $xml;
 
     /**
@@ -18,7 +25,8 @@ class XmlParser
     }
 
     public function parse(){
-
+        $this->removeWhitespaceAndLineBreaks();
+        $this->findRootElement();
     }
 
     /**
@@ -30,10 +38,89 @@ class XmlParser
     }
 
     /**
-     * @param string $xml
+     * @param $xml
+     * @return $this
      */
     public function setXml($xml)
     {
         $this->xml = $xml;
+        return $this;
+    }
+
+    /**
+     * @return XmlElement
+     */
+    public function getRootElement(): XmlElement
+    {
+        return $this->rootElement;
+    }
+
+    /**
+     * @param XmlElement $rootElement
+     */
+    public function setRootElement(XmlElement $rootElement)
+    {
+        $this->rootElement = $rootElement;
+    }
+
+    public function removeWhitespaceAndLineBreaks(){
+        $this->xml = preg_replace("/\n/", "", $this->xml);
+    }
+
+    private function findRootElement() {
+        $rootTag = $this->getFirstTag($this->xml);
+
+        $children = $this->getChildren($this->getXmlBetweenTag($rootTag, $this->xml));
+        $attributes = $this->getFirstTagAttributes($this->xml);
+        $this->rootElement = new XmlElement($rootTag, $children, $attributes);
+        $this->getChildrenData($this->rootElement->getChildren(), $this->getXmlBetweenTag($rootTag, $this->xml));
+    }
+    private function getFirstTag($xml) {
+        preg_match("/<(\w[^(><.)]*) ?.*>/", $xml, $elements);
+        return explode(' ', $elements[1])[0];
+    }
+    private function getFirstTagAttributes($xml) {
+        preg_match("/<(\w[^(><.)]+) ?.*>/", $xml, $elements);
+        $tagParts = explode(' ', $elements[1]);
+        unset($tagParts[0]);
+        $attributes = [];
+        foreach($tagParts as $attribute){
+            preg_match("/(\w*)=\"?(\w*)\"?/", $attribute, $attributeParts);
+            $attributes[$attributeParts[1]] = $attributeParts[2];
+        }
+        return $attributes;
+    }
+
+    private function getXmlBetweenTag(string $tag, string $xml){
+        preg_match("/<$tag.*?>(.*)<\/$tag>/", $xml,$matches);
+        return $matches[1];
+    }
+    private function getChildren($xml){
+        $childTags = [];
+            while(strlen($xml)>0) {
+
+                $tag = $this->getFirstTag($xml);
+                if($tag == ""){
+                    break;
+                }
+                $childTags[] = new XmlElement($tag);
+                $xml = preg_replace("/<$tag(.*)<\/$tag>/", "", $xml);
+            }
+        return $childTags;
+    }
+    private function getChildrenData(Array $elements, $parentInnerXml) {
+        foreach($elements as $element){
+            $tag = $element->getTag();
+            $elementXml = $this->getXmlBetweenTag($tag, $parentInnerXml);
+            $children = $this->getChildren($elementXml);
+            if(count($children)==0){
+                $element->setValue($elementXml);
+            } else {
+                $attributes = $this->getFirstTagAttributes($parentInnerXml);
+                $element->setChildren($children);
+                $element->setAttributes($attributes);
+                $this->getChildrenData($children, $elementXml);
+            }
+        }
     }
 }
--- tests/XmlParserTest.php
+++ tests/XmlParserTest.php
@@ -18,7 +18,7 @@ final class XmlParserTest extends TestCase
     }
 
     /**
-     * @dataProvider XmlProvider
+     * @dataProvider XmlFirstElementProvider
      */
     public function testIfConstructorInputIsReturnedInGetter($xml) {
         $xmlParser = new XmlParser($xml);
@@ -26,7 +26,7 @@ final class XmlParserTest extends TestCase
     }
 
     /**
-     * @dataProvider XmlProvider
+     * @dataProvider XmlFirstElementProvider
      */
     public function testIfSetterInputIsReturnedInGetter($xml) {
         $this->xmlParser->setXml($xml);
@@ -34,15 +34,68 @@ final class XmlParserTest extends TestCase
     }
 
     /**
-     * @dataProvider XmlProvider
+     * @dataProvider XmlFirstElementProvider
      */
-    public function testIfFirstElementObjectIsCorrect($xml) {
-       $this->xmlParser->setXml = $xml;
+    public function testIfFirstElementObjectIsCorrect($xml, $rootElementTag) {
+       $this->xmlParser->setXml($xml)->parse();
+       $this->assertEquals($this->xmlParser->getRootElement()->getTag(), $rootElementTag);
+
+    }
+    /**
+     * @dataProvider WhitespaceProvider
+     */
+    public function testIfLineBrakesAreRemoved($xml, $expected){
+        $this->xmlParser->setXml($xml)->parse();
+        $this->assertEquals($expected, $this->xmlParser->getXml());
+
+    }
+    /**
+     * @dataProvider XmlFirstElementChildrenProvider
+     */
+    public function testIfFirstElmentChildrenTagsAreCorrect($xml, $expectedChildrenTags){
+        $this->xmlParser->setXml($xml)->parse();
+        $actualChildrenTags = [];
+        foreach($this->xmlParser->getRootElement()->getChildren() as $child){
+            $actualChildrenTags[] = $child->getTag();
+        }
+        $this->assertEquals($expectedChildrenTags, $actualChildrenTags);
     }
 
-    public function XmlProvider() {
+    /**
+     * @dataProvider XmlAttributeProvider
+     */
+    public function testIfAttributesAreCorrect($xml, $expectedAttributes){
+        $this->xmlParser->setXml($xml)->parse();
+        $actualAttributes = $this->xmlParser->getRootElement()->getChildren()[0]->getAttributes();
+
+        $this->assertEquals($expectedAttributes, $actualAttributes);
+    }
+    public function XmlFirstElementProvider() {
         return [
-            'one tag only' => ['<test></test>'],
+            'one tag only' => ['<test></test>', 'test'],
+            'exercise example' => ['<apply><csymbol encoding="OpenMath"><msub><mi>P</mi><mn>1</mn></msub></csymbol><ci>x</ci></apply>', 'apply'],
             ];
+    }
+    public function XmlFirstElementChildrenProvider() {
+        return [
+            'one tag only' => ['<test></test>', []],
+            'exercise example' => ['<apply><csymbol encoding="OpenMath"><msub><mi>P</mi><mn>1</mn></msub></csymbol><ci>x</ci></apply>', ['csymbol', 'ci']],
+            '3 tags' => ['<test><a>a</a><b>b</b><n>c</n></test>', ['a', 'b', 'n']],
+        ];
+    }
+    public function XmlAttributeProvider() {
+        return [
+            'exercise example' => ['<apply><csymbol encoding="OpenMath"><msub><mi>P</mi><mn>1</mn></msub></csymbol><ci>x</ci></apply>', ['encoding' => 'OpenMath']],
+            'multiple attributes' => ['<apply><csymbol encoding="OpenMath" emoticon="lol" funny="very"><msub><mi>P</mi><mn>1</mn></msub></csymbol><ci>x</ci></apply>', ['encoding' => 'OpenMath', 'emoticon' => 'lol', 'funny'=>'very']],
+        ];
+    }
+    public function WhitespaceProvider() {
+        return [
+//            'whitespace' => ['<test> </test>', '<test></test>'],
+//            'exercise example' => ['<apply><csymbol encoding="OpenMath"> <msub><mi>P</mi><mn>1</mn> </msub> </csymbol><ci>x</ci></apply>', '<apply><csymbol encoding="OpenMath"><msub><mi>P</mi><mn>1</mn></msub></csymbol><ci>x</ci></apply>'],
+//            'multiple spaces' => ['<test>   <some>    </some>   </test>', '<test><some></some></test>'],
+//            'tabs' => ["<test>\t<some>\t \t</some>\t\t</test>", '<test><some></some></test>'],
+            'line brakes' => ["\n<test>\n<some>\n \n</some>\n\n</test>\n", '<test><some> </some></test>'],
+        ];
     }
 }
